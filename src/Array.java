f37968f889936eae93925aeebe72272c2ed3b0cd
huangguorui <2315684336@qq.com>
2021-05-19 07:23:34 +0800

    add Array.java

---
+++ src/Array.java
@@ -0,0 +1,28 @@
+public class Array {
+    //私有变量，不允许用户从外部来进行获取
+    private int[] data;
+    private int size;
+    //构造函数，传入数组的容量capacity构造Array
+    public Array(int capacity){
+        data = new int[capacity];
+        size = 0;
+    }
+
+    //无参数的构造函数，默认数组的容量capacity=10
+    public Array(){
+        this(10);
+    }
+    //获取数组中的元素个数
+    public int getSize(){
+        return size;
+    }
+    //获取数组的容量
+    public int getCapacity(){
+        return data.length;
+    }
+    //返回数组是否为空
+    public boolean isEmpty(){
+        return size==0;
+    }
+
+}
