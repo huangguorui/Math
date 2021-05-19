09d60a1a1c7d773c68d92ea0ad3d4de1fbea4922
huangguorui <2315684336@qq.com>
2021-05-19 08:25:07 +0800

    add test1

--- src/Application.java
+++ src/Application.java
@@ -1,7 +1,8 @@
 public class Application {
     public static void main(String[] args) {
         System.out.println("Application");
-        System.out.println("test");
+        System.out.println("test1");
+        System.out.println("aad test1");
     }
     public void add(){
 
