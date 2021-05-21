fa526c15622028c833d84503c9f33150c6d4c008
huangguorui <2315684336@qq.com>
2021-05-21 15:18:39 +0800

    2-3 栈的另一个应用：括号匹配 leetcode-cn.com第20题 	有效的括号

---
+++ src/Stack/p3_2_23/Solution.java
@@ -0,0 +1,34 @@
+package Stack.p3_2_23;
+
+import java.util.Stack;
+
+class Solution {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.isValid("()[]{}"));
+
+    }
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(' || c == '[' || c == '{')
+                stack.push(c);
+            else {
+                if (stack.isEmpty())
+                    return false;
+
+                char topChar = stack.pop();
+                if (c == ')' && topChar != '(')
+                    return false;
+                if (c == ']' && topChar != '[')
+                    return false;
+                if (c == '}' && topChar != '{')
+                    return false;
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
