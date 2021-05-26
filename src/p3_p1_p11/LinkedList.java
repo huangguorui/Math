d0c69f0192fd7bed05e6cac5ecb965adfed57c42
huangguorui <2315684336@qq.com>
2021-05-26 19:41:40 +0800

    3-1-1 什么是链表 数组队列和循环队列的比较

---
+++ src/p3_p1_p11/LinkedList.java
@@ -0,0 +1,28 @@
+package p3_p1_p11;
+
+public class LinkedList<E> {
+    private class  Node{
+        public E e;
+        public Node next;
+
+        public Node(E e,Node next){
+            this.e = e;
+            this.next = next;
+        }
+
+        public Node(E e){
+            this(e,null);
+        }
+
+        public Node(){
+            this(null,null);
+        }
+
+        @Override
+        public String toString(){
+            return e.toString();
+        }
+
+    }
+
+}
