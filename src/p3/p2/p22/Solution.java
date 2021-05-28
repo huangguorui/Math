6d875319fbcd1fa51acaaaccdf377ba1359cd30a
huangguorui <2315684336@qq.com>
2021-05-28 10:06:06 +0800

    3-2-2 测试自己的Leetcode链表代码

---
+++ src/p3/p2/p22/Solution.java
@@ -0,0 +1,40 @@
+package p3.p2.p22;
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+
+        while (head != null && head.val == val)
+            head = head.next;
+        if (head == null)
+            return head;
+
+        ListNode prev = head;
+        while (prev.next != null) {
+            if (prev.next.val == val)
+                prev.next = prev.next.next;
+            else
+                prev = prev.next;
+        }
+        return head;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1,2,6,3,4,5,6};
+        ListNode head = new ListNode(nums);
+        System.out.println(head);
+        ListNode res = (new Solution()).removeElements(head,6);
+        System.out.println(res);
+    }
+
+}
