21b4d46cb7fb8cc89444bed107ce7bc6802f8801
huangguorui <2315684336@qq.com>
2021-06-02 10:24:48 +0800

    4-1-8 归并排序法的复杂度分析

---
+++ src/p4/p1/p18/SortingHelper.java
@@ -0,0 +1,50 @@
+package p4.p1.p18;
+
+
+import p4.p1.p14.InsertionSort;
+import p4.p1.p14.SelectionSort;
+
+public class SortingHelper {
+    private SortingHelper(){}
+
+    /**
+     *
+     * @param arr
+     * @param <E>
+     * @return
+     */
+    public static <E extends Comparable<E>> boolean isSorted(E[] arr){
+        for (int i = 1; i < arr.length; i++)
+            //判断排序是否是正确的
+            if(arr[i-1].compareTo(arr[i]) > 0)
+                return false;
+        return true;
+    }
+    /**
+     *
+     */
+    public static  <E extends Comparable<E>> void  sortTest(String sortname ,E[] arr){
+
+
+        long startTime = System.nanoTime();
+        if(sortname.equals("SelectionSort"))
+            SelectionSort.sort(arr);
+        else if(sortname.equals("InsertionSort"))
+            InsertionSort.sort(arr);
+        else if(sortname.equals("MergeSort"))
+            MergeSort.sort(arr);
+//        else if(sortname.equals("InsertionSort2"))
+//            InsertionSort.sort2(arr);
+        long endTime = System.nanoTime();
+
+        double time = (endTime - startTime) / 1000000000.0;
+
+        if(!SortingHelper.isSorted(arr))
+            throw new RuntimeException(sortname + " failed");
+//        System.out.println(time + " s");
+        System.out.println(String.format("%s , n = %d : %f s", sortname , arr.length,time));
+
+    }
+
+
+}
