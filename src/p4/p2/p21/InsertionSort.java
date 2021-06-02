13e953ac69a5de7cabaed4291e8d6c3b91a92a2c
huangguorui <2315684336@qq.com>
2021-06-02 11:26:16 +0800

    4-2-1 在有序数组，让归并排序法成为 O(n) 算法

---
+++ src/p4/p2/p21/InsertionSort.java
@@ -0,0 +1,51 @@
+package p4.p2.p21;
+
+import java.util.Arrays;
+
+public class InsertionSort {
+    private InsertionSort() {
+    }
+
+
+
+    public static <E extends Comparable<E>> void sort(E[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            //将arr[i]插入到合适的位置
+            E t = arr[i];
+            int j;
+            for (j = i;j - 1 >= 0 && t.compareTo(arr[j - 1]) < 0; j--) {
+                arr[j] = arr[j - 1];
+            }
+            arr[j] = t;
+
+        }
+    }
+
+
+    private static <E> void swap(E[] arr, int i, int j) {
+        E t = arr[i];
+        arr[i] = arr[j];
+        arr[j] = t;
+    }
+    public static void main(String[] args) {
+        int[] dataSize = {10000,100000};
+        for(int n : dataSize){
+            System.out.println("Random Array ：");
+            Integer[] arr = ArrayGenerator.generateRandomArray(n,n);
+            Integer[] arr2 = Arrays.copyOf(arr,arr.length);
+            SortingHelper.sortTest("InsertionSort",arr);
+            SortingHelper.sortTest("SelectionSort",arr2);
+
+            System.out.println();
+
+            System.out.println("Ordered Array : ");
+
+            arr = ArrayGenerator.generateOrderedArray(n);
+            arr2 = Arrays.copyOf(arr,arr.length);
+
+            SortingHelper.sortTest("InsertionSort",arr);
+            SortingHelper.sortTest("SelectionSort",arr2);
+        }
+    }
+
+}
